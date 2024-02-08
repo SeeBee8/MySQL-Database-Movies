a59247d4ed40452451571f6d47dad9605db9eda6
Christina Brockway <141530991+SeeBee8@users.noreply.github.com>
2024-02-07 23:17:50 -0600

    created part 6

---
+++ my_functions.py
@@ -0,0 +1,251 @@
+### CLASSIFICATION METRICS
+
+from sklearn.metrics import classification_report, ConfusionMatrixDisplay
+import matplotlib.pyplot as plt
+import numpy as np
+def classification_metrics(y_true, y_pred, label='',
+                           output_dict=False, figsize=(8,4),
+                           normalize='true', cmap='Blues',
+                           colorbar=False,values_format=".2f"):
+    """Modified version of classification metrics function from Intro to Machine Learning.
+    Updates:
+    - Reversed raw counts confusion matrix cmap  (so darker==more).
+    - Added arg for normalized confusion matrix values_format
+    """
+    # Get the classification report
+    report = classification_report(y_true, y_pred)
+    
+    ## Print header and report
+    header = "-"*70
+    print(header, f" Classification Metrics: {label}", header, sep='\n')
+    print(report)
+    
+    ## CONFUSION MATRICES SUBPLOTS
+    fig, axes = plt.subplots(ncols=2, figsize=figsize)
+    
+    # Create a confusion matrix  of raw counts (left subplot)
+    ConfusionMatrixDisplay.from_predictions(y_true, y_pred,
+                                            normalize=None, 
+                                            cmap='gist_gray_r',# Updated cmap
+                                            values_format="d", 
+                                            colorbar=colorbar,
+                                            ax = axes[0]);
+    axes[0].set_title("Raw Counts")
+    
+    # Create a confusion matrix with the data with normalize argument 
+    ConfusionMatrixDisplay.from_predictions(y_true, y_pred,
+                                            normalize=normalize,
+                                            cmap=cmap, 
+                                            values_format=values_format, #New arg
+                                            colorbar=colorbar,
+                                            ax = axes[1]);
+    axes[1].set_title("Normalized Confusion Matrix")
+    
+    # Adjust layout and show figure
+    fig.tight_layout()
+    plt.show()
+    
+    # Return dictionary of classification_report
+    if output_dict==True:
+        report_dict = classification_report(y_true, y_pred, output_dict=True)
+        return report_dict
+
+
+### Evaluate Classification Networks
+
+def evaluate_classification_network(model, 
+                                    X_train=None, y_train=None, 
+                                    X_test=None, y_test=None,
+                                    history=None, history_figsize=(6,6),
+                                    figsize=(6,4), normalize='true',
+                                    output_dict = False,
+                                    cmap_train='Blues',
+                                    cmap_test="Reds",
+                                    values_format=".2f", 
+                                    colorbar=False):
+    """Evaluates a neural network classification task using either
+    separate X and y arrays or a tensorflow Dataset
+    
+    Data Args:
+        X_train (array, or Dataset)
+        y_train (array, or None if using a Dataset
+        X_test (array, or Dataset)
+        y_test (array, or None if using a Dataset)
+        history (history object)
+        """
+    # Plot history, if provided
+    if history is not None:
+        plot_history(history, figsize=history_figsize)
+    ## Adding a Print Header
+    print("\n"+'='*80)
+    print('- Evaluating Network...')
+    print('='*80)
+    ## TRAINING DATA EVALUATION
+    # check if X_train was provided
+    if X_train is not None:
+        ## Check if X_train is a dataset
+        if hasattr(X_train,'map'):
+            # If it IS a Datset:
+            # extract y_train and y_train_pred with helper function
+            y_train, y_train_pred = get_true_pred_labels(model, X_train)
+        else:
+            # Get predictions for training data
+            y_train_pred = model.predict(X_train)
+        ## Pass both y-vars through helper compatibility function
+        y_train = convert_y_to_sklearn_classes(y_train)
+        y_train_pred = convert_y_to_sklearn_classes(y_train_pred)
+        
+        # Call the helper function to obtain regression metrics for training data
+        results_train = classification_metrics(y_train, y_train_pred, 
+                                         output_dict=True, figsize=figsize,
+                                             colorbar=colorbar, cmap=cmap_train,
+                                               values_format=values_format,
+                                         label='Training Data')
+        
+        ## Run model.evaluate         
+        print("\n- Evaluating Training Data:")
+        print(model.evaluate(X_train, return_dict=True))
+    
+    # If no X_train, then save empty list for results_train
+    else:
+        results_train = []
+    ## TEST DATA EVALUATION
+    # check if X_test was provided
+    if X_test is not None:
+        ## Check if X_train is a dataset
+        if hasattr(X_test,'map'):
+            # If it IS a Datset:
+            # extract y_train and y_train_pred with helper function
+            y_test, y_test_pred = get_true_pred_labels(model, X_test)
+        else:
+            # Get predictions for training data
+            y_test_pred = model.predict(X_test)
+        ## Pass both y-vars through helper compatibility function
+        y_test = convert_y_to_sklearn_classes(y_test)
+        y_test_pred = convert_y_to_sklearn_classes(y_test_pred)
+        
+        # Call the helper function to obtain regression metrics for training data
+        results_test = classification_metrics(y_test, y_test_pred, 
+                                         output_dict=True, figsize=figsize,
+                                             colorbar=colorbar, cmap=cmap_test,
+                                              values_format=values_format,
+                                         label='Test Data')
+        
+        ## Run model.evaluate         
+        print("\n- Evaluating Test Data:")
+        print(model.evaluate(X_test, return_dict=True))
+      
+    # If no X_test, then save empty list for results_test
+    else:
+        results_test = []
+      
+    # Store results in a dictionary
+    results_dict = {'train':results_train,
+                    'test': results_test}
+    if output_dict == True:
+        return results_dict
+
+
+### Plot History
+
+def plot_history(history,figsize=(6,8)):
+    # Get a unique list of metrics 
+    all_metrics = np.unique([k.replace('val_','') for k in history.history.keys()])
+    # Plot each metric
+    n_plots = len(all_metrics)
+    fig, axes = plt.subplots(nrows=n_plots, figsize=figsize)
+    axes = axes.flatten()
+    # Loop through metric names add get an index for the axes
+    for i, metric in enumerate(all_metrics):
+        # Get the epochs and metric values
+        epochs = history.epoch
+        score = history.history[metric]
+        # Plot the training results
+        axes[i].plot(epochs, score, label=metric, marker='.')
+        # Plot val results (if they exist)
+        try:
+            val_score = history.history[f"val_{metric}"]
+            axes[i].plot(epochs, val_score, label=f"val_{metric}",marker='.')
+        except:
+            pass
+        finally:
+            axes[i].legend()
+            axes[i].set(title=metric, xlabel="Epoch",ylabel=metric)
+    # Adjust subplots and show
+    fig.tight_layout()
+    plt.show()
+
+
+### Convert y to Sklearn Classes
+
+def convert_y_to_sklearn_classes(y, verbose=False):
+    # If already one-dimension
+    if np.ndim(y)==1:
+        if verbose:
+            print("- y is 1D, using it as-is.")
+        return y
+        
+    # If 2 dimensions with more than 1 column:
+    elif y.shape[1]>1:
+        if verbose:
+            print("- y is 2D with >1 column. Using argmax for metrics.")   
+        return np.argmax(y, axis=1)
+    
+    else:
+        if verbose:
+            print("y is 2D with 1 column. Using round for metrics.")
+        return np.round(y).flatten().astype(int)
+
+
+### GET TRUE PRED LABELS
+
+def get_true_pred_labels(model,ds):
+    """Gets the labels and predicted probabilities from a Tensorflow model and Dataset object.
+    Adapted from source: https://stackoverflow.com/questions/66386561/keras-classification-report-accuracy-is-different-between-model-predict-accurac
+    """
+    y_true = []
+    y_pred_probs = []
+    
+    # Loop through the dataset as a numpy iterator
+    for images, labels in ds.as_numpy_iterator():
+        
+        # Get prediction with batch_size=1
+        y_probs = model.predict(images, batch_size=1, verbose=0)
+        # Combine previous labels/preds with new labels/preds
+        y_true.extend(labels)
+        y_pred_probs.extend(y_probs)
+    ## Convert the lists to arrays
+    y_true = np.array(y_true)
+    y_pred_probs = np.array(y_pred_probs)
+    
+    return y_true, y_pred_probs
+
+## Build LSTM MODEL
+
+# Define a function for building an LSTM model
+from tensorflow.keras.models import Sequential
+from tensorflow.keras import layers, optimizers, regularizers
+def build_lstm_model(text_vectorization_layer):
+    
+    # Define sequential model with pre-trained vectorization layer and *new* embedding layer
+    lstm_model = Sequential([
+        text_vectorization_layer,
+        layers.Embedding(input_dim=VOCAB_SIZE,
+                                  output_dim=EMBED_DIM, 
+                                  input_length=SEQUENCE_LENGTH)
+        ])
+        
+    # Add *new* LSTM layer
+    lstm_model.add(layers.LSTM(128))
+    # Add output layer
+    lstm_model.add(layers.Dense(len(classes), activation='softmax'))
+ 
+    # Compile the model
+    lstm_model.compile(optimizer=optimizers.legacy.Adam(learning_rate = .01), 
+                  loss='sparse_categorical_crossentropy', 
+                  metrics=['accuracy'])
+    
+    lstm_model.summary()
+    return lstm_model
+
+
