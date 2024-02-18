100b5106e176f8d0eac3750953e3e423e27bb9d5
Christina Brockway <141530991+SeeBee8@users.noreply.github.com>
2024-02-18 17:07:29 -0600

    created streamlit app

--- movies_streamlit.py
+++ movies_streamlit.py
@@ -4,46 +4,68 @@ import streamlit.components.v1 as components
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import os
+import os, sys
 import joblib
-
+import tensorflow as tf
 from sklearn.metrics import classification_report, ConfusionMatrixDisplay
 from lime.lime_text import LimeTextExplainer
-
+import stdfunctions as sf
 import json
+
 with open('config/filepaths.json') as f:
     FPATHS = json.load(f)
-st.title("Predicting Yelp Review Ratings")
+    
+st.title("Predicting Movie Review Ratings")
+
+@st.cache_data
+def load_data(fpath):
+    df = pd.read_csv(fpath)
+    return df
 
 
-# Get text to predict from the text input box
-X_to_pred = st.text_input("### Enter text to predict here:", 
-                          value="Best movie ever!")
+## Loading our training and test data
+@st.cache_data
+def load_Xy_data(joblib_fpath):
+    return joblib.load(joblib_fpath)
+    
+# Load & cache dataframe
+df = load_data(fpath = FPATHS['data']['filtered'])    
+# Load training data from FPATHS
+train_data_fpath  = FPATHS['data']['ml']['train']
+X_train, y_train = load_Xy_data(train_data_fpath)
+# Load test data from FPATHS
+test_data_fpath  = FPATHS['data']['ml']['test']
+X_test, y_test = load_Xy_data(test_data_fpath)
 
-# Loading the ML model
+## Loading the ML model
 @st.cache_resource
 def load_ml_model(fpath):
     loaded_model = joblib.load(fpath)
     return loaded_model
-# Load model from FPATHS
-model_fpath = FPATHS['models']['rf']
-clf_pipe = load_ml_model(model_fpath)
+
+fpath_model = FPATHS['models']['rf']
+clf_pipe = load_ml_model(fpath_model)
 
 # load target lookup dict
 @st.cache_data
 def load_lookup(fpath=FPATHS['data']['ml']['target_lookup']):
     return joblib.load(fpath)
+
+target_lookup = load_lookup()
+
+
 @st.cache_resource
 def load_encoder(fpath=FPATHS['data']['ml']['label_encoder'] ):
     return joblib.load(fpath)
 
-# Load the target lookup dictionary
-target_lookup = load_lookup()
-target_lookup
-
 # Load the encoder
 encoder = load_encoder()
-encoder.classes_
+
+
+# Get text to predict from the text input box
+X_to_pred = st.text_input("### Enter text to predict here:", 
+                          value="I loved the movie and the action! Great storyline." )
+
 
 # Basic Function to obtain prediction
 def make_prediction(X_to_pred, clf_pipe=clf_pipe, lookup_dict= target_lookup):
@@ -52,13 +74,14 @@ def make_prediction(X_to_pred, clf_pipe=clf_pipe, lookup_dict= target_lookup):
     # Decode label
     pred_class = lookup_dict[pred_class]
     return pred_class
+    
+    
 # Trigger prediction with a button
 if st.button("Get prediction."):
     pred_class = make_prediction(X_to_pred)
     st.markdown(f"##### Predicted category:  {pred_class}") 
 else: 
     st.empty()
-
 
 @st.cache_resource
 def get_explainer(class_names = None):
@@ -71,51 +94,41 @@ def explain_instance(explainer, X_to_pred, predict_func):
 # Create the lime explainer
 explainer = get_explainer(class_names = encoder.classes_)
 
-## Loading our training and test data
-@st.cache_data
-def load_Xy_data(joblib_fpath):
-    return joblib.load(joblib_fpath)
-# Load training data from FPATHS
-train_data_fpath  = FPATHS['data']['ml']['train']
-X_train, y_train = load_Xy_data(train_data_fpath)
-# Load test data from FPATHS
-test_data_fpath  = FPATHS['data']['ml']['test']
-X_test, y_test = load_Xy_data(test_data_fpath)
+# Obtaining an explanation for our X_to_pred text
+explanation = explain_instance(explainer, X_to_pred, predict_func=clf_pipe.predict_proba)
+# To display in the notebook
+from IPython.display import display, HTML
+display(HTML(explanation))
+
+st.divider()
 
 ##To place the 3 checkboxes side-by-side
 col1,col2,col3 = st.columns(3)
 show_train = col1.checkbox("Show training data.", value=True)
 show_test = col2.checkbox("Show test data.", value=True)
 show_model_params =col3.checkbox("Show model params.", value=False)
-if st.button("Show model evaluation."):
-    pass # placeholder
-
-
 
 if st.button("Show model evaluation."):
-
     if show_train == True:
-    # Display training data results
-    y_pred_train = clf_pipe.predict(X_train)
-    report_str, conf_mat = classification_metrics_streamlit(y_train, y_pred_train,
-                                                            label='Training Data')
-    st.text(report_str)
-    st.pyplot(conf_mat)
-    st.text("\n\n")
+        # Display training data results
+        y_pred_train = clf_pipe.predict(X_train)
+        report_str, conf_mat = sf.classification_metrics_streamlit(y_train,
+        y_pred_train,label='TrainingData')
+        st.text(report_str)
+        st.pyplot(conf_mat)
+        st.text("\n\n")
     if show_test == True: 
-    # Display the trainin data resultsg
-    y_pred_test = clf_pipe.predict(X_test)
-    report_str, conf_mat = classification_metrics_streamlit(y_test, y_pred_test,
-                                                            cmap='Reds',label='TestData')
-    st.text(report_str)
-    st.pyplot(conf_mat)
-    st.text("\n\n")
+        # Display the trainin data resultsg
+        y_pred_test = clf_pipe.predict(X_test)
+        report_str, conf_mat = sf.classification_metrics_streamlit(y_test,
+                    y_pred_test, cmap='Reds',label='TestData')
+        st.text(report_str)
+        st.pyplot(conf_mat)
+        st.text("\n\n")
     
     if show_model_params:
-    # Display model params
-    st.markdown("####  Model Parameters:")
-    st.write(clf_pipe.get_params())
+        # Display model params
+        st.markdown("####  Model Parameters:")
+        st.write(clf_pipe.get_params())
 else: st.empty()
-
-
 
--- stdfunctions.py
+++ stdfunctions.py
@@ -397,3 +397,50 @@ def evaluate_regression(reg, X_train, y_train, X_test, y_test, verbose = True,
     # Return the dataframe
     return results_df.round(3)
 
+
+from sklearn.metrics import classification_report, ConfusionMatrixDisplay
+import matplotlib.pyplot as plt
+import numpy as np
+def classification_metrics_streamlit(y_true, y_pred, label='',
+                           figsize=(8,4),
+                           normalize='true', cmap='Blues',
+                           colorbar=False,values_format=".2f",
+                                    class_names=None):
+    """Modified version of classification metrics function from Intro to Machine Learning.
+    Updates:
+    - Reversed raw counts confusion matrix cmap  (so darker==more).
+    - Added arg for normalized confusion matrix values_format
+    """
+    # Get the classification report
+    report = classification_report(y_true, y_pred,target_names=class_names)
+    
+    ## Save header and report
+    header = "-"*70
+    final_report = "\n".join([header,f" Classification Metrics: {label}", header,report,"\n"])
+        
+    ## CONFUSION MATRICES SUBPLOTS
+    fig, axes = plt.subplots(ncols=2, figsize=figsize)
+    
+    # Create a confusion matrix  of raw counts (left subplot)
+    ConfusionMatrixDisplay.from_predictions(y_true, y_pred,
+                                            normalize=None, 
+                                            cmap='gist_gray_r',# Updated cmap
+                                            display_labels = class_names, # Added display labels
+                                            values_format="d", 
+                                            colorbar=colorbar,
+                                            ax = axes[0]);
+    axes[0].set_title("Raw Counts")
+    
+    # Create a confusion matrix with the data with normalize argument 
+    ConfusionMatrixDisplay.from_predictions(y_true, y_pred,
+                                            normalize=normalize,
+                                            cmap=cmap, 
+                                            values_format=values_format, #New arg
+                                            display_labels = class_names, # Added display labels
+                                            colorbar=colorbar,
+                                            ax = axes[1]);
+    axes[1].set_title("Normalized Confusion Matrix")
+    
+    # Adjust layout and show figure
+    fig.tight_layout()
+    return final_report, fig
