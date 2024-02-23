0e256e265c3007677a870c38f98014ac3d07c58a
Christina Brockway <141530991+SeeBee8@users.noreply.github.com>
2024-02-22 22:13:05 -0600

    update

--- main_page.py
+++ main_page.py
@@ -15,16 +15,16 @@ st.markdown(
 import streamlit.components.v1 as components 
 # Source: https://meta.stackoverflow.com/questions/392785/need-to-add-linkedin-and-github-badges-in-profile-page-of-stack-overflow
 # Source: https://docs.streamlit.io/library/api-reference/layout/st.container
-with st.container(border=True, height = 200):
-    st.markdown("***Brought to you By: Christina Brockway***")
-    st.markdown("**Contact Me!**")
-    links_html = """<ul>
-    <li><a href="csbrockway602@gmail.com">Email</a></li>
-      <li><a href="https://www.linkedin.com/in/christina-brockway/[removed]" rel="nofollow noreferrer">
+st.divider()
+st.markdown("***Brought to you By: Christina Brockway***")
+st.markdown("**Contact Me!**")
+links_html = """<ul>
+<li><a href="csbrockway602@gmail.com">Email</a></li>
+    <li><a href="https://www.linkedin.com/in/christina-brockway/[removed]" rel="nofollow noreferrer">
         <img src="https://i.stack.imgur.com/gVE0j.png" alt="linkedin"> LinkedIn
-      </a> </li>
-      <li><a href="https://github.com/dashboard" rel="nofollow noreferrer">
+     </a> </li>
+     <li><a href="https://github.com/dashboard" rel="nofollow noreferrer">
         <img src="https://i.stack.imgur.com/tskMh.png" alt="github"> Github
-      </a></li>
-    </ul>"""
-    components.html(links_html)
+    </a></li>
+</ul>"""
+components.html(links_html)
